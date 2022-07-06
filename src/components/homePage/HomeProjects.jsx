72c27526662123468c0fc3c3b1bc35b3bb2bb63a
Richard D Shin <shin.richard.d@gmail.com>
2022-07-06 10:19:04 -0400

    adjsuted

--- src/components/homePage/HomeProjects.jsx
+++ src/components/homePage/HomeProjects.jsx
@@ -4,6 +4,7 @@ import { motion } from "framer-motion";
 import styled from "styled-components";
 import { Container, Flex } from "../../styles/globalStyles";
 import SectionHeader from "../SectionHeader";
+import projectImg from "../../assets/images/projects-img.png";
 
 const StyledHomeProjects = styled.div`
     padding: 100px 0;
@@ -15,14 +16,18 @@ const ProjectsGrid = styled.div`
     /* border-radius: 20px; */
 `;
 
-const Project = styled(motion.div)`
+const Project = styled(motion.a)`
     cursor: pointer;
-    border: 1px solid black;
+    /* border: 1px solid black; */
     width: 350px;
     height: 250px;
 
     /**TEMP */
     background: white;
+`;
+
+const IMG = styled.img`
+    width: 350px;
 `;
 
 const HomeProjects = () => {
@@ -32,7 +37,16 @@ const HomeProjects = () => {
                 <Flex center column fullHeight>
                     <SectionHeader>Projects</SectionHeader>
                     <ProjectsGrid>
-                        <Project whileHover={{ scale: 1.05 }}></Project>
+                        <Project
+                            whileHover={{ scale: 1.05 }}
+                            href="https://ty4coffee.thekima.com/"
+                            target="_blank"
+                        >
+                            <IMG src={projectImg} />
+                            <p style={{ fontSize: 14 }}>
+                                Genshin Wish Simulator
+                            </p>
+                        </Project>
                         <Project whileHover={{ scale: 1.05 }}></Project>
                         <Project whileHover={{ scale: 1.05 }}></Project>
                         <Project whileHover={{ scale: 1.05 }}></Project>
