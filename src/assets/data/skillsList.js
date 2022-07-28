42b1aa82689274d3f785f676ad009ee2afab0c3c
Richard D Shin <shin.richard.d@gmail.com>
2022-07-28 16:06:09 -0400

    moved skillsList in HomeAbout to asset data folder

---
+++ src/assets/data/skillsList.js
@@ -0,0 +1,61 @@
+export const skillsList = [
+    {
+        name: "CSS, HTML, Javascript",
+        progress: 100,
+        content:
+            "The basics of web development, utilizing these are a second-nature. I'm using to making complex css UI components",
+    },
+    {
+        name: "React",
+        progress: 95,
+        content:
+            "As a long time React developer, I am very knowledgeable with React and React Native, from React Hooks to React Context for state management.",
+    },
+    {
+        name: "Typescript",
+        progress: 90,
+        content:
+            "I have picked up Typescript quite recently, and I understand more than enough to utilize it to its full potential.",
+    },
+    {
+        name: "Firebase",
+        progress: 90,
+        content:
+            "Fireebase has been my friend for many projects. This includes hosting, cloud storage, cloud functions, and tracking database for users and information alike.",
+    },
+    {
+        name: "Amazon AWS",
+        progress: 90,
+        content:
+            "Using S3 and Cloudfront CDN, I have managed massive projects with thousands of images for fast and cheap content delivery. That includes my Genshin Wish Simulator! It delivers hundreds of assets from characters and weapons and UI from my simulator to internationally all around the globe.",
+    },
+    {
+        name: "Styled Components",
+        progress: 95,
+        content:
+            "Styled Components are my all-time favorite. Whether it's for themes, Framer Motion, or just straight up styling, I am completely comfortable with this library.",
+    },
+    {
+        name: "Gatsby",
+        progress: 95,
+        content:
+            "Gatbsy has been my go-to framework. I am very well-aware of it's capabilities and use it for most of my recent applications.",
+    },
+    {
+        name: "Express",
+        progress: 85,
+        content: "I have used Express.js a few times for old projects.",
+    },
+    {
+        name: "Material UI",
+        progress: 100,
+        content:
+            "Very easy and straightforward framework. Although I don't like its outdated design, I am able to use it to create fast mock-up applications.",
+    },
+    {
+        name: "Framer Motion",
+        progress: 100,
+        content:
+            "I am obsessed with Framer Motion. As someone who loves UI, UX, and design, being able to design motions on web is the icing on the cake. I am no stranger to this animation library.",
+    },
+];
